4f8737d8b721b50a54af07d27a5ec832e92cdde3
sonictw <sonictw2@gmail.com>
2023-12-22 16:31:34 +0900

    product

--- src/main/java/bt/product/controller/ProductController.java
+++ src/main/java/bt/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,8 +15,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import bt.btframework.utils.BMap;
 import bt.btframework.utils.BReqData;
 import bt.btframework.utils.BRespData;
-import bt.btframework.utils.LoginInfo;
-import bt.common.service.CommonService;
 import bt.product.service.ProductService;
 
 @Controller
@@ -24,168 +23,180 @@ public class ProductController {
 	
 	@Resource(name = "ProductService")
 	private ProductService productService;
-	
-	@Resource(name = "CommonService")
-	private CommonService commonService;
-	
-	
+
 	@RequestMapping(value = "/product/Product.do")
-	public String product() throws Exception {
+	public String product(ModelMap model) throws Exception {
+		model.addAttribute("basyy", productService.selectBasYY(null));
+		model.addAttribute("season", productService.selectSeason(null));
 		return "/product/Product";
 	}
 	
-	@RequestMapping(value = "/popup/productPopUp.do")
-	public String productPopUp() throws Exception {
-		return "/popup/ProductPopUp";
+	@RequestMapping(value = "/popup/ProductDetailPopUp.do")
+	public String ProductDetailPopUp(ModelMap model) throws Exception {
+		model.addAttribute("basyy", productService.selectBasYY(null));
+		model.addAttribute("season", productService.selectSeason(null));
+		model.addAttribute("hdng", productService.selectHdng(null));
+		return "/popup/ProductDetailPopUp";
 	}
-
-	@RequestMapping(value = "/popup/itemPopUp.do")
-	public String itemPopUp() throws Exception {
-		return "/popup/ItemPopUp";
+	
+	@RequestMapping(value = "/popup/ProductPeriodPopUp.do")
+	public String ProductPeriodPopUp(ModelMap model) throws Exception {
+		model.addAttribute("basyy", productService.selectBasYY(null));
+		return "/popup/ProductPeriodPopUp";
+	}
+	
+	@RequestMapping(value = "/popup/ProductPeriodDetailPopUp.do")
+	public String ProductPeriodDetailPopUp(ModelMap model) throws Exception {
+		model.addAttribute("season", productService.selectSeason(null));
+		return "/popup/ProductPeriodDetailPopUp";
+	}
+	
+	@RequestMapping(value = "/popup/ProductCopyPopUp.do")
+	public String ProductCopyPopUp(ModelMap model) throws Exception {
+		model.addAttribute("basyy", productService.selectBasYY(null));
+		return "/popup/ProductCopyPopUp";
+	}
+	
+	@RequestMapping(value = "/popup/ProductSelectPeriodPopUp.do")
+	public String ProductSelectPeriodPopUp(ModelMap model) throws Exception {
+		return "/popup/ProductSelectPeriodPopUp";
 	}
 	
 	/**
-	 * Product 조회.
+	 * Product Info 조회
 	 * @param reqData
 	 * @param req
 	 * @return
 	 * @throws Exception
 	 */
-	@RequestMapping(value = "/product/selectProductInfo.do", method = RequestMethod.POST)
+	@RequestMapping(value = "/common/selectProductInfo.do", method = RequestMethod.POST)
 	@ResponseBody
-	public BRespData selectNoticeList(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
-		BMap param = reqData.getParamDataMap("param");
+	public BRespData selectProductInfo(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
+		BMap param = new BMap();
+		param.put("SSN_GBN", reqData.get("SSN_GBN"));
+		param.put("ST_DT1", reqData.get("ST_DT1"));
+		
 		BRespData respData = new BRespData();
 		
-		BMap resultProductInfo = productService.selectProductInfo(param);
-		
-		if(null == resultProductInfo || resultProductInfo.isEmpty()){
-			respData.put("resultProductInfo", null);
-		}else{
-			respData.put("resultProductInfo", resultProductInfo);
+		respData.put("result", productService.selectProductInfo(param));
+		return respData;
+	}
+	
+	/**
+	 * Product Info 저장
+	 * @param reqData
+	 * @param req
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/common/saveProductInfo.do", method = RequestMethod.POST)
+	@ResponseBody
+	public BRespData saveProductInfo(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
+		BMap param = reqData.getParamDataMap("param");
+		BRespData respData = new BRespData();
+
+		if(!productService.saveProductInfo(param)){
+			respData.put("SAVE", "N");
 		}
 		
-		
 		return respData;
 	}
+	
+	/**
+	 * Product Info 삭제
+	 * @param reqData
+	 * @param req
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/common/deleteProductInfo.do", method = RequestMethod.POST)
+	@ResponseBody
+	public BRespData deleteProductInfo(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
+		BMap param = reqData.getParamDataMap("param");
+		BRespData respData = new BRespData();
+		
+		productService.deleteProductInfo(param);
+		
+		return respData;
+	}	
+	
+	
+	/**
+	 * Period Info 조회
+	 * @param reqData
+	 * @param req
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/common/selectPeriodInfo.do", method = RequestMethod.POST)
+	@ResponseBody
+	public BRespData selectPeriodInfo(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
+		BMap param = new BMap();
+		param.put("ST_DT1", reqData.get("ST_DT1"));
+		
+		BRespData respData = new BRespData();
+		
+		respData.put("result", productService.selectPeriodInfo(param));
+		return respData;
+	}
+	
+	/**
+	 * Period Info 저장
+	 * @param reqData
+	 * @param req
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/common/savePeriodInfo.do", method = RequestMethod.POST)
+	@ResponseBody
+	public BRespData savePeriodInfo(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
+		BMap param = reqData.getParamDataMap("param");
+		BRespData respData = new BRespData();
 
-	/**
-	 * Product Desc List 조회.
-	 * @param reqData
-	 * @param req
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping(value = "/product/selectDescList.do", method = RequestMethod.POST)
-	@ResponseBody
-	public BRespData selectDescList(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
-		BMap param = reqData.getParamDataMap("param");
-		BRespData respData = new BRespData();
-		
-		respData.put("result", productService.selectDescList(param));
-		
-		return respData;
-	}
-	
-	/**
-	 * Product Unit of Measure List 조회.
-	 * @param reqData
-	 * @param req
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping(value = "/product/selectUOMList.do", method = RequestMethod.POST)
-	@ResponseBody
-	public BRespData selectUOMList(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
-		BMap param = reqData.getParamDataMap("param");
-		BRespData respData = new BRespData();
-		
-		respData.put("result", productService.selectUOMList(param));
-		
-		return respData;
-	}
-	
-	/**
-	 * Product Basic Data Text List 조회.
-	 * @param reqData
-	 * @param req
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping(value = "/product/selectBasicList.do", method = RequestMethod.POST)
-	@ResponseBody
-	public BRespData selectBasicList(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
-		BMap param = reqData.getParamDataMap("param");
-		BRespData respData = new BRespData();
-		
-		respData.put("result", productService.selectBasicList(param));
-		
-		return respData;
-	}
-	
-	/**
-	 * Product PopUp List 조회.
-	 * @param reqData
-	 * @param req
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping(value = "/product/selectMatlPopList.do", method = RequestMethod.POST)
-	@ResponseBody
-	public BRespData selectMatlPopList(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
-		BMap param = reqData.getParamDataMap("param");
-		
-		BRespData respData = new BRespData();
-		
-		respData.put("result", productService.selectMatlPopList(param));
-		
-		return respData;
-	}
-	
-	/**
-	 * Product Code Check
-	 * @param model
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping(value = "/product/chkMatlCd.do", method = RequestMethod.POST)
-	@ResponseBody
-	public BRespData chkMatlCd(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception {
-		
-		BMap param = reqData.getParamDataMap("param");
-		BRespData respData = new BRespData();
-		
-		String tempVal = productService.chkMatlCd(param);
-		
-		if(null != tempVal && !"".equals(tempVal)){
-			if(1 < Integer.parseInt(tempVal)){
-				respData.put("isOk", "N");
-			}else{
-				respData.put("isOk", "Y");
-			}
-		}else{
-			respData.put("isOk", "N");
+		if(!productService.savePeriodInfo(param)){
+			respData.put("SAVE", "N");
 		}
+		
 		return respData;
 	}
 	
 	/**
-	 * Product Data 조회.
+	 * Period Info 삭제
 	 * @param reqData
 	 * @param req
 	 * @return
 	 * @throws Exception
 	 */
-	@RequestMapping(value = "/product/getMatlData.do", method = RequestMethod.POST)
+	@RequestMapping(value = "/common/deletePeriodInfo.do", method = RequestMethod.POST)
 	@ResponseBody
-	public BRespData getMatlData(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
+	public BRespData deletePeriodInfo(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
 		BMap param = reqData.getParamDataMap("param");
-		param.put("G_COMP_CD", LoginInfo.getCompCd());
-		
 		BRespData respData = new BRespData();
 		
-		respData.put("result", productService.getMatlData(param));
+		productService.deletePeriodInfo(param);
+		
+		return respData;
+	}	
+	
+	
+	/**
+	 * Period Copy 등록
+	 * @param reqData
+	 * @param req
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/common/saveCopyInfo.do", method = RequestMethod.POST)
+	@ResponseBody
+	public BRespData saveCopyInfo(@RequestBody BReqData reqData, HttpServletRequest req) throws Exception{
+		BMap param = reqData.getParamDataMap("param");
+		BRespData respData = new BRespData();
+		
+		if(!productService.saveCopyInfo(param)){
+			respData.put("SAVE", "N");
+		}
 		
 		return respData;
 	}
+	
 }
