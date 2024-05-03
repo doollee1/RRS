1b4ebe486a58688fd48742d079203df614499f5d
DOOLLEE2 <DOOLLEE2@DESKTOP-DQPTKO8>
2024-05-03 15:37:16 +0900

    일반(02) 예약가능(03) 일시 메일전송

--- src/main/java/bt/common/service/MailSendService.java
+++ src/main/java/bt/common/service/MailSendService.java
@@ -35,6 +35,9 @@ public class MailSendService {
 	 * @exception Exception
 	 */
 	public boolean sendMail(BMap param) throws Exception {
+		
+		logger.info("=========== 메일전송 첨부파일 포함 =============");
+		
 //		String sender = param.getString("SENDER"); // 수신자
 //		String receiver = param.getString("RECEIVER"); // 수신자
 //		String carbonCopy = param.getString("CARBON_COPY"); // 수신자
@@ -93,5 +96,43 @@ public class MailSendService {
 		return true;
 	}
 
+	
+	public boolean sendMailNoAttach(BMap param) throws Exception {
+		
+		logger.info("=========== 메일전송 첨부파일 없음 =============");
+			
+		try {
+		    String toEmail = (String) param.get("TO_EMAIL");
+		    String msg = (String) param.get("MSG");
+		    String subject = (String) param.get("SUBJECT");
+		    
+		   
+		    // 기본 메일 정보를 생성합니다
+		    MultiPartEmail email = new MultiPartEmail();
+		    email.setCharset("euc-kr");// 한글 인코딩
+		    email.setHostName(env.getProperty("EMAIL.HOST"));
+            email.setSmtpPort(Integer.parseInt(env.getProperty("EMAIL.PORT")));
+            email.setSslSmtpPort(env.getProperty("EMAIL.PORT"));
+            email.setAuthenticator(new DefaultAuthenticator(env.getProperty("EMAIL.FROM"), env.getProperty("EMAIL.PW"))); // common.properties, "skEnffl!2#4" 
+            email.setSSLOnConnect(true);
+            
+		    email.addTo(toEmail);
+		    email.setFrom(env.getProperty("EMAIL.FROM"));
+		    
+		    email.setSubject(subject);
+		    email.setMsg(msg);
+		    
+		    // 메일을 전송합니다
+		    email.send();
+
+
+		} catch (EmailException  ex) {
+			logger.info("EmailException ::::: " + ex.getMessage());
+			ex.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
 	
 }
